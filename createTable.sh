3515b7be649ef14336498ab198292892efff8016
Mustafa-Ashraf751 <mustafaashraf123@yahoo.com>
2025-02-01 11:10:29 +0200

    refactor code and add some constraints

--- createTable.sh
+++ createTable.sh
@@ -46,33 +46,48 @@ while true; do
         dialog --msgbox "Invalid column name!" 10 40
         continue
       fi
-      col_type=$(dialog --inputbox "Enter column type for col $i:" 10 40 --output-fd 1)
-      if [[ -z $col_type ]]; then
-        dialog --msgbox "Column type cannot be empty!" 10 40
-        continue
+      #let the user choose the data type
+      choice=$(dialog --title "Please choose data type of table  $TB_Name" --menu "choose an option:" 15 50 6 \1 "String" \
+      2 "Number" \
+      3 "Email"  \
+      4 "Date" --output-fd 1)
+      EXIT_STATUS=$?
+
+      if [ $EXIT_STATUS -ne 0 ]; then 
+      dialog --msgbox "Going back to table menu ..." 10 40
+      return 1
       fi
-      if [[ ! $col_type =~ ^[a-zA-Z0-9_()]+$ ]]; then
-        dialog --msgbox "Invalid column type!" 10 40
-        continue
+     
+     case $choice in 
+      1) col_type=string;;
+      2) col_type=number;;
+      3) col_type=email;;
+      4) col_type=date;;
+      esac
+      
+      #let the user choose the constraints
+      choice=$(dialog --title "Please choose data type of table  $TB_Name" --menu "choose an option:" 15 55 6 \1 "Primary Key (must choose at least one column)" \
+      2 "Not Null" \
+      3 "Unique"  \
+      4 "None" --output-fd 1)
+      EXIT_STATUS=$?
+
+      if [ $EXIT_STATUS -ne 0 ]; then 
+      dialog --msgbox "Going back to table menu ..." 10 40
+      return 1
       fi
-      if [  $primaryKey -eq 0 ]; then
-        while true; do
-          check=$(dialog --inputbox "Do you want to make this column primary key for table $TB_Name (yes/no):" 10 40 --output-fd 1)
-          if [[ $check == "yes" ]]; then
-          primaryKey=1
-          tempCol="$col_name:$col_type:primary"
-          break
-          elif [[ $check == "no" ]]; then
-          tempCol="$col_name:$col_type"
-          break
-          else  
-          dialog --msgbox "Invalid input please try again!" 10 40
-          fi
-        done
-     else 
-      tempCol="$col_name:$col_type"
-     fi
-     columns+=("$tempCol")
+     
+     case $choice in 
+      1) col_constraint=primaryKey
+       primaryKey=1;;
+      2) col_constraint=notNull;;
+      3) col_constraint=unique;;
+      4) col_constraint=none;;
+      esac
+
+      tempCol="$col_name:$col_type:$col_constraint"
+
+      columns+=("$tempCol")
   done
   #Make sure that table has primary key
   if [ $primaryKey -eq 0 ]; then
